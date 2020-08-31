b5ac4e5f00191b679524c44fc007aa06d4e8c6d0
napiorkowskik83 <napiorkowskik83@gmail.com>
2020-08-31 17:08:16 +0200

    script.js update (httpS)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://glacial-refuge-75135.herokuapp.com/v1/task/';
+  var apiRoot = 'https://glacial-refuge-75135.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
